2e13ac1ff8cb69c5b26db1d3248d27bb83ff924b
timbset <timbset@gmail.com>
2020-02-03 16:10:42 +0300

    Group helpers by aws resource

---
+++ src/aws/iam/putRolePolicy.ts
@@ -0,0 +1,47 @@
+import IAM from 'aws-sdk/clients/iam'
+
+import { retry, Options, getLog, Log } from '../../utils'
+
+interface TMethod {
+  (
+    params: {
+      Region: string
+      PolicyName: string
+      RoleName: string
+      PolicyDocument: {
+        Version: string
+        Statement: Array<{
+          Action: string | Array<string>
+          Resource: string | Array<string>
+          Effect: 'Allow' | 'Deny'
+        }>
+      }
+    },
+    log?: Log
+  ): Promise<void>
+}
+
+const putRolePolicy: TMethod = async (
+  { Region, RoleName, PolicyName, PolicyDocument },
+  log = getLog('PUT-ROLE-POLICY')
+) => {
+  const iam = new IAM({ region: Region })
+
+  try {
+    log.debug(`Put the role "${RoleName}" policy "${PolicyName}"`)
+
+    const ensureRolePolicy = retry(iam, iam.putRolePolicy, Options.Defaults.override({ log }))
+    await ensureRolePolicy({
+      PolicyName,
+      RoleName,
+      PolicyDocument: JSON.stringify(PolicyDocument)
+    })
+  } catch (error) {
+    log.debug(`Failed to put the role "${RoleName}" policy "${PolicyName}"`)
+    throw error
+  }
+
+  log.debug(`The role "${RoleName}" policy "${PolicyName}" has been putted`)
+}
+
+export default putRolePolicy
---
+++ src/aws/lambda/updateFunctionCode.ts
@@ -0,0 +1,43 @@
+import Lambda from 'aws-sdk/clients/lambda'
+
+import { retry, Options, getLog, Log } from '../../utils'
+
+interface TMethod {
+  (
+    params: {
+      Region: string
+      FunctionName: string
+      S3Bucket: string
+      S3Key: string
+    },
+    log?: Log
+  ): Promise<void>
+}
+
+const updateFunctionCode: TMethod = async (
+  { Region, FunctionName, S3Bucket, S3Key },
+  log: Log = getLog('UPDATE-FUNCTION-CODE')
+) => {
+  try {
+    log.debug(`Update the function "${FunctionName}" code`)
+
+    const lambda = new Lambda({ region: Region })
+
+    const setFunctionCode = retry(
+      lambda,
+      lambda.updateFunctionCode,
+      Options.Defaults.override({ log, silent: true, maxAttempts: 1 })
+    )
+    await setFunctionCode({
+      FunctionName,
+      S3Bucket,
+      S3Key
+    })
+    log.debug(`The function "${FunctionName}" code has been updated`)
+  } catch (error) {
+    log.debug(`Failed to update the function "${FunctionName}" code`)
+    throw error
+  }
+}
+
+export default updateFunctionCode
