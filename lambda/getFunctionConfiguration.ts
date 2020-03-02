5ca596ff3ba366dd4a8f8f2103e6b2990b84c638
Georgy K <36447502+Lykoi18@users.noreply.github.com>
2020-03-02 14:57:17 +0300

    Add getFunctionConfiguration (#4)

---
+++ lambda/getFunctionConfiguration.ts
@@ -0,0 +1,47 @@
+import Lambda from 'aws-sdk/clients/lambda'
+
+import { retry, Options, getLog, Log } from '../utils'
+
+interface TMethod {
+  (
+    params: {
+      Region: string
+      FunctionName: string
+    },
+    log?: Log
+  ): Promise<any>
+}
+
+const getFunctionConfiguration: TMethod = async (
+  { Region, FunctionName },
+  log = getLog('GET-FUNCTION-CONFIGURATION')
+) => {
+  try {
+    const lambda = new Lambda({ region: Region })
+
+    log.debug(`Get current function environment variables`)
+
+    const getFunctionConfig = retry(
+      lambda,
+      lambda.getFunctionConfiguration,
+      Options.Defaults.override({ log })
+    )
+    const functionConfiguration = await getFunctionConfig({
+      FunctionName,
+      Qualifier: '$LATEST'
+    })
+
+    if (functionConfiguration == null) {
+      throw new Error('Function configuration not found')
+    }
+
+    log.debug(`Function configuration have been got`)
+
+    return functionConfiguration
+  } catch (error) {
+    log.debug(`Failed to get function configuration`)
+    throw error
+  }
+}
+
+export default getFunctionConfiguration
