f59f8fd4672cda1620bf23acef52d9899890b29d
Georgy K <36447502+Lykoi18@users.noreply.github.com>
2020-04-17 13:51:23 +0300

    Improve postgres (#17)

--- postgres/index.ts
+++ postgres/index.ts
@@ -2,5 +2,16 @@ import escapeStr from './escapeStr'
 import escapeId from './escapeId'
 import executeStatement from './executeStatement'
 import createStatementExecutor from './createStatementExecutor'
+import beginTransaction from './beginTransaction'
+import commitTransaction from './commitTransaction'
+import rollbackTransaction from './rollbackTransaction'
 
-export { escapeStr, escapeId, executeStatement, createStatementExecutor }
+export {
+  escapeStr,
+  escapeId,
+  executeStatement,
+  createStatementExecutor,
+  beginTransaction,
+  commitTransaction,
+  rollbackTransaction
+}
