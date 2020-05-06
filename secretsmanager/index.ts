d8e4031c88b9192abeff71b633617333a959123e
Lykoi18 <aiden.ghost666@gmail.com>
2020-05-06 18:03:48 +0300

    Add getSecret

--- secretsmanager/index.ts
+++ secretsmanager/index.ts
@@ -1,5 +1,6 @@
 import createSecret from './createSecret'
 import deleteSecret from './deleteSecret'
 import generatePostgresPassword from './generatePostgresPassword'
+import getSecret from './getSecret'
 
-export { createSecret, deleteSecret, generatePostgresPassword }
+export { createSecret, deleteSecret, generatePostgresPassword, getSecret }
