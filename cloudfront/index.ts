64626b13dc2d10974dd4ffd9520c6510bda92bc9
mrcheater <mister.cheater@list.ru>
2020-09-09 16:12:09 +0300

    Add "getCloudFrontDistributionsByTags"

--- cloudfront/index.ts
+++ cloudfront/index.ts
@@ -2,6 +2,7 @@ import createCloudFrontDistribution from './createCloudFrontDistribution'
 import createCloudFrontOriginAccessIdentity from './createCloudFrontOriginAccessIdentity'
 import getCloudFrontDistributionArnByTags from './getCloudFrontDistributionArnByTags'
 import getCloudFrontDistributionById from './getCloudFrontDistributionById'
+import getCloudFrontDistributionsByTags from './getCloudFrontDistributionsByTags'
 import getCloudFrontOriginAccessIdentity from './getCloudFrontOriginAccessIdentity'
 import listCloudFrontOriginAccessIdentities from './listCloudFrontOriginAccessIdentities'
 import updateCloudFrontDistribution from './updateCloudFrontDistribution'
@@ -11,6 +12,7 @@ export {
   createCloudFrontOriginAccessIdentity,
   getCloudFrontDistributionArnByTags,
   getCloudFrontDistributionById,
+  getCloudFrontDistributionsByTags,
   getCloudFrontOriginAccessIdentity,
   listCloudFrontOriginAccessIdentities,
   updateCloudFrontDistribution
