418faa5f3278ccd83231c39e1b3f67f9eceba7c1
mrcheater <mister.cheater@list.ru>
2020-09-10 16:35:57 +0300

    Improve find by tags

--- lambda/getFunctionByTags.ts
+++ lambda/getFunctionByTags.ts
@@ -8,7 +8,7 @@ async function getFunctionByTags(
     Tags: { [key: string]: string }
   },
   log: Log = getLog('GET-FUNCTION-BY-TAGS')
-): Promise<string> {
+): Promise<string | null> {
   const { Region, Tags } = params
 
   const TagFilters = Object.entries(Tags).map(([Key, Value]) => ({ Key, Values: [Value] }))
@@ -35,7 +35,7 @@ async function getFunctionByTags(
   }
 
   if (resources == null || resources.length === 0) {
-    throw new Error('Resource Not Found')
+    return null
   }
   if (resources.length > 1) {
     log.verbose(resources.map(({ ResourceARN }) => ResourceARN).filter((arn) => arn != null))
@@ -44,7 +44,7 @@ async function getFunctionByTags(
 
   const { ResourceARN } = resources[0]
   if (ResourceARN == null) {
-    throw new Error('Resource Not Found')
+    return null
   }
 
   log.verbose(ResourceARN)
