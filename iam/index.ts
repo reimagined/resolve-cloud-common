e44183bfabfd48c92ae92c34e6ea6c8b3fb628fa
Lykoi18 <aiden.ghost666@gmail.com>
2020-11-05 12:58:37 +0300

    Implement deleteRolePolicy

--- iam/index.ts
+++ iam/index.ts
@@ -1,5 +1,6 @@
 import deleteRole from './deleteRole'
+import deleteRolePolicy from './deleteRolePolicy'
 import ensureRoleWithPolicy from './ensureRoleWithPolicy'
 import putRolePolicy from './putRolePolicy'
 
-export { deleteRole, ensureRoleWithPolicy, putRolePolicy }
+export { deleteRole, deleteRolePolicy, ensureRoleWithPolicy, putRolePolicy }
