98867535f7e1c9b567dc7027128fa9f8143aa94a
Lykoi18 <aiden.ghost666@gmail.com>
2020-11-17 12:24:19 +0300

    Implement set/delete function tags

---
+++ lambda/deleteFunctionTags.ts
@@ -0,0 +1,38 @@
+import Lambda from 'aws-sdk/clients/lambda'
+
+import { retry, Options, getLog, Log } from '../utils'
+
+const deleteFunctionTags = async (
+  params: {
+    Region: string
+    FunctionName: string
+    TagKeys: Array<string>
+  },
+  log: Log = getLog('DELETE-FUNCTION-TAGS')
+): Promise<void> => {
+  const { FunctionName, Region, TagKeys } = params
+
+  const lambda = new Lambda({ region: Region })
+
+  const untagResource = retry(
+    lambda,
+    lambda.untagResource,
+    Options.Defaults.override({ log, maxAttempts: 1 })
+  )
+
+  try {
+    log.debug(`Delete the function tags ${TagKeys}`)
+
+    await untagResource({
+      Resource: FunctionName,
+      TagKeys
+    })
+
+    log.debug(`Function tags "${FunctionName}" has been deleted`)
+  } catch (error) {
+    log.error(`Failed to delete the function tags "${FunctionName}"`)
+    throw error
+  }
+}
+
+export default deleteFunctionTags
