4d4edf6dce9219c9da8f936c66c4dc4f1674b1da
timbset <timbset@gmail.com>
2020-11-18 18:36:15 +0300

    Implement putMetricData

---
+++ cloudwatch/putMetricData.ts
@@ -0,0 +1,78 @@
+import CloudWatch from 'aws-sdk/clients/cloudwatch'
+
+import { getLog, Log, Options, retry } from '../utils'
+
+const MAX_METRICS_DIMENSION_VALUE_LENGTH = 256
+
+const trimString = (input: string, maxLength: number): string => {
+  const trimmedStringEnd = '...'
+
+  return input.length > maxLength
+    ? `${input.slice(
+      0,
+      MAX_METRICS_DIMENSION_VALUE_LENGTH - trimmedStringEnd.length
+    )}${trimmedStringEnd}`
+    : input
+}
+
+export const putMetricData = async (
+  params: {
+    Message: string,
+    Resource: string,
+    MetricName: 'Errors' | 'Warnings'
+  },
+  log: Log = getLog('PUT-METRIC-DATA')
+) => {
+  const cw = new CloudWatch()
+
+  try {
+    const now = new Date()
+    const message = trimString(params.Message, MAX_METRICS_DIMENSION_VALUE_LENGTH)
+    const putMetrics = retry(cw, cw.putMetricData, Options.Defaults.override({ log }))
+
+    await putMetrics({
+      Namespace: 'RESOLVE_CLOUD_METRICS',
+      MetricData: [
+        {
+          MetricName: params.MetricName,
+          Timestamp: now,
+          Unit: 'Count',
+          Value: 1,
+          Dimensions: [
+            {
+              Name: 'Resource',
+              Value: params.Resource,
+            },
+            {
+              Name: 'Message',
+              Value: message,
+            },
+          ],
+        },
+        {
+          MetricName: params.MetricName,
+          Timestamp: now,
+          Unit: 'Count',
+          Value: 1,
+          Dimensions: [
+            {
+              Name: 'Resource',
+              Value: params.Resource,
+            },
+          ],
+        },
+        {
+          MetricName: params.MetricName,
+          Timestamp: now,
+          Unit: 'Count',
+          Value: 1
+        },
+      ],
+    })
+  } catch (e) {
+    log.error(e)
+    throw e
+  }
+}
+
+export default putMetricData
