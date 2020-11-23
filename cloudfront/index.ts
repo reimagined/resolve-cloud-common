9da1eee4d4907d814f761426218750a40ab36928
Lykoi18 <aiden.ghost666@gmail.com>
2020-11-23 14:03:00 +0300

    Add set/delete distribution tags

--- cloudfront/index.ts
+++ cloudfront/index.ts
@@ -1,23 +1,27 @@
 import createCloudFrontDistribution from './createCloudFrontDistribution'
 import createCloudFrontOriginAccessIdentity from './createCloudFrontOriginAccessIdentity'
 import deleteCloudFrontDistribution from './deleteCloudFrontDistribution'
+import deleteCloudFrontDistributionTags from './deleteCloudFrontDistributionTags'
 import deleteCloudFrontOriginAccessIdentity from './deleteCloudFrontOriginAccessIdentity'
 import ensureOriginAccessIdentity from './ensureOriginAccessIdentity'
 import getCloudFrontDistributionById from './getCloudFrontDistributionById'
 import getCloudFrontDistributionsByTags from './getCloudFrontDistributionsByTags'
 import getCloudFrontOriginAccessIdentity from './getCloudFrontOriginAccessIdentity'
 import listCloudFrontOriginAccessIdentities from './listCloudFrontOriginAccessIdentities'
+import setCloudFrontDistributionTags from './setCloudFrontDistributionTags'
 import updateCloudFrontDistribution from './updateCloudFrontDistribution'
 
 export {
   createCloudFrontDistribution,
   createCloudFrontOriginAccessIdentity,
   deleteCloudFrontDistribution,
+  deleteCloudFrontDistributionTags,
   deleteCloudFrontOriginAccessIdentity,
   ensureOriginAccessIdentity,
   getCloudFrontDistributionById,
   getCloudFrontDistributionsByTags,
   getCloudFrontOriginAccessIdentity,
   listCloudFrontOriginAccessIdentities,
+  setCloudFrontDistributionTags,
   updateCloudFrontDistribution
 }
