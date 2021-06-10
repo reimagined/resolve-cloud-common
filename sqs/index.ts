f7e7bdff0f1b8daadc5a88eae35920a6cc3d7037
TSPMpix <lok_92@mail.ru>
2021-06-10 15:40:04 +0300

    Fix

--- sqs/index.ts
+++ sqs/index.ts
@@ -1,6 +1,7 @@
 import deleteSqsQueue from './deleteSqsQueue'
 import ensureSqsQueue from './ensureSqsQueue'
+import listSqsQueue from './listSqsQueue'
 import sendMessage from './sendMessage'
 import receiveMessage from './receiveMessage'
 
-export { deleteSqsQueue, ensureSqsQueue, sendMessage, receiveMessage }
+export { deleteSqsQueue, ensureSqsQueue, listSqsQueue, sendMessage, receiveMessage }
