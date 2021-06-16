20e15997af9a77bd2aee19a89c5c5c97f51cead1
TSPMpix <lok_92@mail.ru>
2021-06-16 17:43:26 +0300

    Merge

--- sqs/deleteSqsQueue.ts
+++ sqs/deleteSqsQueue.ts
@@ -58,6 +58,7 @@ const deleteSqsQueue = async (
       }
       log.debug(`SQS queue tags has been deleted`)
     } catch (error) {
+      // eslint-disable-next-line no-console
       console.warn(error)
     }
   } catch (error) {
