566b5c86eeb9fdbccd996d75a8c54378928168cf
TSPMpix <lok_92@mail.ru>
2021-06-16 17:34:04 +0300

    Add error wrapper

--- sqs/deleteSqsQueue.ts
+++ sqs/deleteSqsQueue.ts
@@ -49,10 +49,17 @@ const deleteSqsQueue = async (
 
     const QueueArn = `arn:aws:sqs:${Region}:${Account}:${QueueName}`
 
-    await untagResource({
-      TagKeys: tagKeyList,
-      ResourceARNList: [QueueArn]
-    })
+    try {
+      if (tagKeyList != null) {
+        await untagResource({
+          TagKeys: tagKeyList,
+          ResourceARNList: [QueueArn]
+        })
+      }
+      log.debug(`SQS queue tags has been deleted`)
+    } catch (error) {
+      console.warn(error)
+    }    
   } catch (error) {
     if (IfExist) {
       log.debug(`Skip delete the queue "${QueueName}"`)
