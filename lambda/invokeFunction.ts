b5e8c1f74e5614d30727079279ee4b52718e5ebe
mrcheater <mister.cheater@list.ru>
2021-07-05 12:10:40 +0300

    Improve invokeFunction

--- lambda/invokeFunction.ts
+++ lambda/invokeFunction.ts
@@ -11,12 +11,24 @@ async function invokeFunction<Response extends any>(
     Payload: Record<string, any>
     InvocationType?: 'Event' | 'RequestResponse' | 'DryRun'
     WithLogs?: boolean
+    MaximumExecutionDuration?: number
   },
   log: Log = getLog('INVOKE-FUNCTION')
 ): Promise<Response> {
-  const { Payload, FunctionName, Region, InvocationType, WithLogs } = params
+  const {
+    Payload,
+    FunctionName,
+    Region,
+    InvocationType,
+    WithLogs,
+    MaximumExecutionDuration = 300000
+  } = params
 
-  const lambda = new Lambda({ region: Region, maxRetries: 0 })
+  const lambda = new Lambda({
+    region: Region,
+    maxRetries: 0,
+    httpOptions: { timeout: MaximumExecutionDuration }
+  })
 
   log.debug(`Invoke lambda ${JSON.stringify(FunctionName)}`)
   log.verbose('Payload', Payload)
