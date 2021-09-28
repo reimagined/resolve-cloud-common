8635d8f04b7ab04d575b2425da76c12199016e28
Timofey Traynenkov <timbset@gmail.com>
2021-09-28 17:55:14 +0300

    Fix Statement ID in adding of lambda permission for SNS topic (#147)
    * Fix statement id in adding of lambda permission for sns topic
    
    * Do not expect error on permission adding after it was removed

--- sns/subscribeTopic.ts
+++ sns/subscribeTopic.ts
@@ -1,6 +1,5 @@
 import Sns from 'aws-sdk/clients/sns'
 import Lambda from 'aws-sdk/clients/lambda'
-import { v4 as uuid } from 'uuid'
 
 import { Log, getLog, retry, Options } from '../utils'
 
@@ -19,25 +18,52 @@ const subscribeTopic = async (
   const sns = new Sns({ region: Region })
   const lambda = new Lambda({ region: Region })
   const subscribeSnsTopic = retry(sns, sns.subscribe)
+
   const addPermission = retry(lambda, lambda.addPermission, Options.Defaults.override({ log }))
 
-  try {
-    if (Endpoint.startsWith('arn:aws:lambda:')) {
-      log.verbose('Endpoint is detected as lambda. Adding permissions')
+  const removePermission = retry(
+    lambda,
+    lambda.removePermission,
+    Options.Defaults.override({ log, expectedErrors: ['ResourceNotFoundException'] })
+  )
 
+  const statementId = TopicArn.split(':')[5]
+
+  if (Endpoint.startsWith('arn:aws:lambda:')) {
+    log.verbose('Endpoint is detected as lambda. Removing old lambda permission')
+
+    try {
+      await removePermission({
+        FunctionName: Endpoint,
+        StatementId: statementId
+      })
+
+      log.verbose('Old permission removed')
+    } catch (error) {
+      if (error.name !== 'ResourceNotFoundException') {
+        log.verbose('Removing old permission failed')
+        throw error
+      }
+
+      log.verbose('Old permission not found')
+    }
+
+    log.verbose('Adding lambda permission')
+
+    try {
       await addPermission({
         FunctionName: Endpoint,
-        StatementId: uuid(),
+        StatementId: statementId,
         Action: 'lambda:InvokeFunction',
         Principal: 'sns.amazonaws.com',
         SourceArn: TopicArn
       })
 
-      log.verbose('Lambda permissions added')
+      log.verbose('Lambda permission added')
+    } catch (error) {
+      log.verbose('Adding permission failed')
+      throw error
     }
-  } catch (error) {
-    log.verbose('SNS topic subscribing failed')
-    throw error
   }
 
   try {
