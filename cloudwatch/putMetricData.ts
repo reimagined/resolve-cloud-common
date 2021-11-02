d5e0449cc8cefe7e3945666f3819d010dd58f490
Lykoi18 <aiden.ghost666@gmail.com>
2021-11-02 12:54:39 +0300

    Fix putMetricData

--- cloudwatch/putMetricData.ts
+++ cloudwatch/putMetricData.ts
@@ -1,73 +1,21 @@
-import CloudWatch from 'aws-sdk/clients/cloudwatch'
+import CloudWatch, { PutMetricDataInput } from 'aws-sdk/clients/cloudwatch'
 
 import { getLog, Log, Options, retry } from '../utils'
 
-const MAX_METRICS_DIMENSION_VALUE_LENGTH = 256
-
-const trimString = (input: string, maxLength: number): string => {
-  const trimmedStringEnd = '...'
-
-  return input.length > maxLength
-    ? `${input.slice(
-        0,
-        MAX_METRICS_DIMENSION_VALUE_LENGTH - trimmedStringEnd.length
-      )}${trimmedStringEnd}`
-    : input
-}
-
 export const putMetricData = async (
-  params: {
-    Message: string
-    Resource: string
-    MetricName: 'Errors' | 'Warnings'
-  },
+  params: PutMetricDataInput,
   log: Log = getLog('PUT-METRIC-DATA')
 ): Promise<void> => {
+  const { Namespace, MetricData } = params
+
   const cw = new CloudWatch()
 
   try {
-    const now = new Date()
-    const message = trimString(params.Message, MAX_METRICS_DIMENSION_VALUE_LENGTH)
     const putMetrics = retry(cw, cw.putMetricData, Options.Defaults.override({ log }))
 
     await putMetrics({
-      Namespace: 'RESOLVE_CLOUD_METRICS',
-      MetricData: [
-        {
-          MetricName: params.MetricName,
-          Timestamp: now,
-          Unit: 'Count',
-          Value: 1,
-          Dimensions: [
-            {
-              Name: 'Resource',
-              Value: params.Resource
-            },
-            {
-              Name: 'Message',
-              Value: message
-            }
-          ]
-        },
-        {
-          MetricName: params.MetricName,
-          Timestamp: now,
-          Unit: 'Count',
-          Value: 1,
-          Dimensions: [
-            {
-              Name: 'Resource',
-              Value: params.Resource
-            }
-          ]
-        },
-        {
-          MetricName: params.MetricName,
-          Timestamp: now,
-          Unit: 'Count',
-          Value: 1
-        }
-      ]
+      Namespace,
+      MetricData
     })
   } catch (e) {
     log.error(e)
